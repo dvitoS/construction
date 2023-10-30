98d24ad2935454a5047ac4e115bb5fcb11eac059
dvitoS <vito.skender@gmail.com>
2023-10-30 22:21:42 +0100

    "workers list changed"

--- src/app/pages/workers/list/ListWorkersPage.tsx
+++ src/app/pages/workers/list/ListWorkersPage.tsx
@@ -3,12 +3,16 @@ import clsx from 'clsx'
 import React, {useState} from 'react'
 import {KTIcon, toAbsoluteUrl} from '../../../../_metronic/helpers'
 import {getLayoutFromLocalStorage, ILayout, LayoutSetup} from '../../../../_metronic/layout/core'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 const ListWorkersPage: React.FC = () => {
   const [tab, setTab] = useState('Sidebar')
   const [config, setConfig] = useState<ILayout>(getLayoutFromLocalStorage())
   const [configLoading, setConfigLoading] = useState<boolean>(false)
   const [resetLoading, setResetLoading] = useState<boolean>(false)
+  const [data, setData] = useState<any[]>([])
+
 
   const updateConfig = () => {
     setConfigLoading(true)
@@ -28,334 +32,47 @@ const ListWorkersPage: React.FC = () => {
       setResetLoading(false)
     }, 1000)
   }
+
+  useEffect(()=> {
+    axios.get('https://phpstack-675879-3984600.cloudwaysapps.com/api/v1/constructions.json')
+    .then(res => setData(res.data))
+    .catch(err => console.log(err));
+  }, [])
   
   return (
-    <div>
-      <div className='card-body py-3'>
-        {/* begin::Table container */}
-        <div className='table-responsive'>
-          {/* begin::Table */}
-          <table className='table align-middle gs-0 gy-4'>
-            {/* begin::Table head */}
-            <thead>
-              <tr className='fw-bold text-muted bg-light'>
-                <th className='ps-4 min-w-325px rounded-start'>Product</th>
-                <th className='min-w-125px'>Price</th>
-                <th className='min-w-125px'>Deposit</th>
-                <th className='min-w-200px'>Agent</th>
-                <th className='min-w-150px'>Status</th>
-                <th className='min-w-200px text-end rounded-end'></th>
-              </tr>
-            </thead>
-            {/* end::Table head */}
-            {/* begin::Table body */}
-            <tbody>
-              <tr>
-                <td>
-                  <div className='d-flex align-items-center'>
-                    <div className='symbol symbol-50px me-5'>
-                      <img
-                        src={toAbsoluteUrl('/media/stock/600x400/img-26.jpg')}
-                        className=''
-                        alt=''
-                      />
-                    </div>
-                    <div className='d-flex justify-content-start flex-column'>
-                      <a href='#' className='text-dark fw-bold text-hover-primary mb-1 fs-6'>
-                        Sant Extreanet Solution
-                      </a>
-                      <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                        HTML, JS, ReactJS
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $2,790
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Paid</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $520
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Rejected</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    Bradly Beal
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Insurance</span>
-                </td>
-                <td>
-                  <span className='badge badge-light-primary fs-7 fw-semibold'>Approved</span>
-                </td>
-                <td className='text-end'>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='switch' className='fs-3' />
-                  </a>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='pencil' className='fs-3' />
-                  </a>
-                  <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'>
-                    <KTIcon iconName='trash' className='fs-3' />
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className='d-flex align-items-center'>
-                    <div className='symbol symbol-50px me-5'>
-                      <img
-                        src={toAbsoluteUrl('/media/stock/600x400/img-3.jpg')}
-                        className=''
-                        alt=''
-                      />
-                    </div>
-                    <div className='d-flex justify-content-start flex-column'>
-                      <a href='#' className='text-dark fw-bold text-hover-primary mb-1 fs-6'>
-                        Telegram Development
-                      </a>
-                      <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                        C#, ASP.NET, MS SQL
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $4,790
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Paid</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $240
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Rejected</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    Chris Thompson
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>NBA Player</span>
-                </td>
-                <td>
-                  <span className='badge badge-light-danger fs-7 fw-semibold'>In Progress</span>
-                </td>
-                <td className='text-end'>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='switch' className='fs-3' />
-                  </a>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='pencil' className='fs-3' />
-                  </a>
-                  <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'>
-                    <KTIcon iconName='trash' className='fs-3' />
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className='d-flex align-items-center'>
-                    <div className='symbol symbol-50px me-5'>
-                      <img
-                        src={toAbsoluteUrl('/media/stock/600x400/img-9.jpg')}
-                        className=''
-                        alt=''
-                      />
-                    </div>
-                    <div className='d-flex justify-content-start flex-column'>
-                      <a href='#' className='text-dark fw-bold text-hover-primary mb-1 fs-6'>
-                        Payroll Application
-                      </a>
-                      <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                        PHP, Laravel, VueJS
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $4,390
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Paid</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $593
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Rejected</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    Zoey McGee
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                    Ruby Developer
-                  </span>
-                </td>
-                <td>
-                  <span className='badge badge-light-success fs-7 fw-semibold'>Success</span>
-                </td>
-                <td className='text-end'>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='switch' className='fs-3' />
-                  </a>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='pencil' className='fs-3' />
-                  </a>
-                  <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'>
-                    <KTIcon iconName='trash' className='fs-3' />
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className='d-flex align-items-center'>
-                    <div className='symbol symbol-50px me-5'>
-                      <img
-                        src={toAbsoluteUrl('/media/stock/600x400/img-18.jpg')}
-                        className=''
-                        alt=''
-                      />
-                    </div>
-                    <div className='d-flex justify-content-start flex-column'>
-                      <a href='#' className='text-dark fw-bold text-hover-primary mb-1 fs-6'>
-                        HR Management System
-                      </a>
-                      <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                        Python, PostgreSQL, ReactJS
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $7,990
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Paid</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $980
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Rejected</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    Brandon Ingram
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Insurance</span>
-                </td>
-                <td>
-                  <span className='badge badge-light-info fs-7 fw-semibold'>Rejected</span>
-                </td>
-                <td className='text-end'>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='switch' className='fs-3' />
-                  </a>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='pencil' className='fs-3' />
-                  </a>
-                  <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'>
-                    <KTIcon iconName='trash' className='fs-3' />
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className='d-flex align-items-center'>
-                    <div className='symbol symbol-50px me-5'>
-                      <img
-                        src={toAbsoluteUrl('/media/stock/600x400/img-8.jpg')}
-                        className=''
-                        alt=''
-                      />
-                    </div>
-                    <div className='d-flex justify-content-start flex-column'>
-                      <a href='#' className='text-dark fw-bold text-hover-primary mb-1 fs-6'>
-                        Telegram Mobile
-                      </a>
-                      <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                        HTML, JS, ReactJS
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $5,790
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Paid</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    $750
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Rejected</span>
-                </td>
-                <td>
-                  <a href='#' className='text-dark fw-bold text-hover-primary d-block mb-1 fs-6'>
-                    Natali Trump
-                  </a>
-                  <span className='text-muted fw-semibold text-muted d-block fs-7'>Insurance</span>
-                </td>
-                <td>
-                  <span className='badge badge-light-warning fs-7 fw-semibold'>Approved</span>
-                </td>
-                <td className='text-end'>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='switch' className='fs-3' />
-                  </a>
-                  <a
-                    href='#'
-                    className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                  >
-                    <KTIcon iconName='pencil' className='fs-3' />
-                  </a>
-                  <a href='#' className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'>
-                    <KTIcon iconName='trash' className='fs-3' />
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-            {/* end::Table body */}
-          </table>
-          {/* end::Table */}
-        </div>
-        {/* end::Table container */}
-      </div>
+    <div className='d-flex flex-column  align-items-center bg-light vh-100'>
+    <h1>Lista gradilišta</h1>
+    <div className='w-75 rounded bg-white border shadow p-4'>
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Ime</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+              data.map((d, i) => (
+                <tr key={i}>
+                  <td>{d.id}</td>
+                  <td>{d.name}</td>
+                  <td>
+                    <button className='btn btn-sm btn-primary me-2'>Read</button>
+                    <button className='btn btn-sm btn-primary me-2'>Edit</button>
+                    <button className='btn btn-sm btn-danger'>Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+        </tbody>
+      </table>
+        
+
     </div>
+  </div>
   )
 }
 
 export {ListWorkersPage}
+
+
