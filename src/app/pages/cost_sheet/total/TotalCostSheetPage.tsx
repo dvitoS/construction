2fd4fd94eb1268e3373618f5fb92d54edc39f36e
dvitoS <vito.skender@gmail.com>
2023-12-10 18:20:50 +0100

    "single worker"

--- src/app/pages/cost_sheet/total/TotalCostSheetPage.tsx
+++ src/app/pages/cost_sheet/total/TotalCostSheetPage.tsx
@@ -22,6 +22,7 @@ const TotalCostSheetPage: React.FC = () => {
   const [checked, setChecked] = useState(true);
   const [data, setData] = useState({name:'', bill:'', note:'' })
   const [total, setTotal] = useState()
+  
 
   const updateConfig = () => {
     setConfigLoading(true)
@@ -58,11 +59,19 @@ const TotalCostSheetPage: React.FC = () => {
     const name = e.target.name;
     const value = e.target.value;
     setData({...data, [name]:value})
-}
+  } 
 
+  enum ConstructionStatus {
+    Active = 0,
+    Inactive = 1,
+    OnHold = 2,
+    Finished = 3,
+  }
 
+  const status: ConstructionStatus = ConstructionStatus.Finished;
 
   return (
+  <div>
     <table className="table align-middle table-row-dashed fs-6 gy-4" id="kt_docs_datatable_subtable">
    {/*  <!--begin::Table head--> */}
     <thead>
@@ -154,7 +163,8 @@ const TotalCostSheetPage: React.FC = () => {
 
         <!--begin::Actions--> */}
         <td className="text-end">
-          <button type="button" className="btn btn-sm btn-icon btn-light btn-active-light-primary toggle h-25px w-25px"
+          <button 
+          type="button" className="btn btn-sm btn-icon btn-light btn-active-light-primary toggle h-25px w-25px"
             data-kt-docs-datatable-subtable="expand_row">
             <span className="svg-icon fs-3 m-0 toggle-off">...</span>
             <span className="svg-icon fs-3 m-0 toggle-on">...</span>
@@ -162,11 +172,119 @@ const TotalCostSheetPage: React.FC = () => {
         </td>
         {/* <!--end::Actions--> */}
       </tr>
-
         ...
     </tbody>
     {/* <!--end::Table body--> */}
   </table>
+  <div>
+      <table className="table align-middle table-row-dashed fs-6 gy-4" id="kt_docs_datatable_subtable">
+     {/*  <!--begin::Table head--> */}
+      <thead>
+        {/* <!--begin::Table row--> */}
+        <tr className="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
+          <th className="min-w-100px">Gradilište</th>
+          <th className="text-end min-w-100px">Početak</th>
+          <th className="text-end min-w-100px">Kraj</th>
+          <th className="text-end min-w-100px">Naplaćeno</th>
+          <th className="text-end min-w-50px">Status</th>
+          <th className="text-end"></th>
+        </tr>
+        {/* <!--end::Table row--> */}
+      </thead>
+      {/* <!--end::Table head--> */}
+
+
+   {/*    <!--begin::Table body--> */}
+      <tbody className="fw-bold text-gray-600">
+        {/* <!--begin::SubTable template--> */}
+        <tr data-kt-docs-datatable-subtable="subtable_template" className="d-none">
+          <td colSpan={2}>
+            <div className="d-flex align-items-center gap-3">
+              <a href="#" className="symbol symbol-50px bg-secondary bg-opacity-25 rounded">
+                <img src="/assets/media/stock/ecommerce/" alt="" data-kt-docs-datatable-subtable="template_image" />
+              </a>
+              <div className="d-flex flex-column text-muted">
+                <a href="#" className="text-gray-900 text-hover-primary fw-bold" data-kt-docs-datatable-subtable="template_name">Product name</a>
+                <div className="fs-7" data-kt-docs-datatable-subtable="template_description">Product description</div>
+              </div>
+            </div>
+          </td>
+          <td className="text-end">
+            <div className="text-gray-900 fs-7">Cost</div>
+            <div className="text-muted fs-7 fw-bold" data-kt-docs-datatable-subtable="template_cost">1</div>
+          </td>
+          <td className="text-end">
+            <div className="text-gray-900 fs-7">Qty</div>
+            <div className="text-muted fs-7 fw-bold" data-kt-docs-datatable-subtable="template_qty">1</div>
+          </td>
+          <td className="text-end">
+            <div className="text-gray-900 fs-7">Total</div>
+            <div className="text-muted fs-7 fw-bold" data-kt-docs-datatable-subtable="template_total">name</div>
+          </td>
+          <td className="text-end">
+            <div className="text-gray-900 fs-7 me-3">On hand</div>
+            <div className="text-muted fs-7 fw-bold" data-kt-docs-datatable-subtable="template_stock">32</div>
+          </td>
+          <td></td>
+        </tr>
+        {/* <!--end::SubTable template--> */}
+
+        <tr>
+          {/* <!--begin::Order ID--> */}
+          <td>
+            <a href="#" className="text-gray-900 text-hover-primary">Ulica grada Vukovara</a>
+          </td>
+          {/* <!--end::Order ID-->
+
+          <!--begin::Crated date--> */}
+          <td className="text-end">
+            10 Nov 2021, 10:30 am
+          </td>
+        {/*   <!--end::Created date-->
+
+          <!--begin::Customer--> */}
+          <td className="text-end">
+            <a href="" className="text-gray-900 text-hover-primary">10 Nov 2022, 12:30 am</a>
+          </td>
+         {/*  <!--end::Customer-->
+
+          <!--begin::Total--> */}
+          <td className="text-end">
+            $630.000
+          </td>
+          {/* <!--end::Total-->
+
+          <!--begin::Profit--> */}
+          <td className="text-end">
+            <span className="text-gray-900 fw-bold">Završeno</span>
+          </td>
+          {/* <!--end::Profit-->
+
+          <!--begin::Status--> */}
+          <td className="text-end">
+            <span className="badge py-3 px-4 fs-7 badge-light-primary">Confirmed</span>
+          </td>
+          {/* <!--end::Status-->
+
+          <!--begin::Actions--> */}
+          <td className="text-end">
+            <button type="button" className="btn btn-sm btn-icon btn-light btn-active-light-primary toggle h-25px w-25px"
+              data-kt-docs-datatable-subtable="expand_row">
+              <span className="svg-icon fs-3 m-0 toggle-off">...</span>
+              <span className="svg-icon fs-3 m-0 toggle-on">...</span>
+            </button>
+          </td>
+          {/* <!--end::Actions--> */}
+        </tr>
+          ...
+      </tbody>
+      {/* <!--end::Table body--> */}
+    </table>
+  </div>
+</div>
+
+
+  
   )
 }
 
--- src/app/pages/workers/singleworkerpage/SingleWorkerPage.tsx
+++ src/app/pages/workers/singleworkerpage/SingleWorkerPage.tsx
@@ -6,6 +6,10 @@ import {getLayoutFromLocalStorage, ILayout, LayoutSetup} from '../../../../_metr
 import axios from 'axios'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import {ChartsWidget1, ListsWidget5, TablesWidget1, TablesWidget5,} from '../../../../_metronic/partials/widgets'
+import {Dropdown1} from '../../../../_metronic/partials'
+import {useLocation} from 'react-router'
+import { useParams } from 'react-router-dom'
 
 
 type Props = {
@@ -18,8 +22,10 @@ const SingleWorkerPage: React.FC<Props> = ({className}) => {
   const [config, setConfig] = useState<ILayout>(getLayoutFromLocalStorage())
   const [configLoading, setConfigLoading] = useState<boolean>(false)
   const [resetLoading, setResetLoading] = useState<boolean>(false)
-  const [data, setData] = useState<any[]>([])
+  const [data, setData] = useState ({firstName:'', lastName:'', address:'', oib:'', email:'', mob:'', passport:'', fatherName:"", motherName:"", workingPermit:'', firstAidDate:'', workProtection:'', firstAid:'', geda:'', hr:'', overtimeHr:'',weekendHr:'',dailyWage:'', wage:'',nightHr:'', note:'', tools:''});
+  const { id } = useParams();
 
+  const location = useLocation()
 
   const updateConfig = () => {
     setConfigLoading(true)
@@ -40,77 +46,320 @@ const SingleWorkerPage: React.FC<Props> = ({className}) => {
     }, 1000)
   }
 
+  function handleSubmit(event:any){
+    event.preventDefault()
+    axios.put('https://phpstack-675879-3984600.cloudwaysapps.com/api/v1/workers/', data)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+    } 
+
+  const handleChangeCheckbox = (e:any) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+  if(e.target.checked == true){
+    setData({...data, [name]:"true"})
+  }else{
+    setData({...data, [name]:""})
+  }
+}
+
+  const handleChange = (e:any) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({...data, [name]:value})
+}
+
   useEffect(()=> {
-    axios.get('https://phpstack-675879-3984600.cloudwaysapps.com/api/v1/workers')
+    axios.get('https://phpstack-675879-3984600.cloudwaysapps.com/api/v1/workers' + data)
     .then(res => setData(res.data))
     .catch(err => console.log(err));
   }, [])
 
-  useEffect(()=> {
-    axios.get('https://phpstack-675879-3984600.cloudwaysapps.com/api/v1/constructions')
+  useEffect(()=>{
+    axios.get('https://phpstack-675879-3984600.cloudwaysapps.com/api/v1/workers/'+ id)
     .then(res => setData(res.data))
     .catch(err => console.log(err));
-  }, [])
-
-
+  },[])
+  const handleInput = (event:any) => {
+  //setData({...data, [event.target.name]: event.target.value})		
+  }
 
   
     return (
+      
+      
     <div>
-      <table className="table table-striped table-rounded border border-gray-300 table-row-bordered table-row-gray-300 gy-7 gs-7">
-        <thead>
-            <tr className="fw-bold fs-4 text-gray-800">
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <td colSpan={4}>
-                    <table className="table table-row-dashed table-row-gray-500 gy-5 gs-5 mb-0">
-                        <thead>
-                            <tr className="fw-bold fs-6 text-gray-800">
-                                <th scope="col">Header</th>
-                                <th scope="col">Header</th>
-                                <th scope="col">Header</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">A</th>
-                                <td>First</td>
-                                <td>Last</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">B</th>
-                                <td>First</td>
-                                <td>Last</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">C</th>
-                                <td>First</td>
-                                <td>Last</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-            </tr>
-        </tbody>
-      </table>
+       <div className='card mb-5 mb-xl-10'>
+        <div className='card-body pt-9 pb-0'>
+          <div className='d-flex flex-wrap flex-sm-nowrap mb-3'>
+            <div className='me-7 mb-4'>
+              <div className='symbol symbol-100px symbol-lg-160px symbol-fixed position-relative'>
+                <img src={toAbsoluteUrl('/media/avatars/300-1.jpg')} alt='Metronic' />
+                <div className='position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-white h-20px w-20px'></div>
+              </div>
+            </div>
+
+            <div className='flex-grow-1'>
+              <div className='d-flex justify-content-between align-items-start flex-wrap mb-2'>
+                <div className='d-flex flex-column'>
+                  <div className='d-flex align-items-center mb-2'>
+                    <a href='#' className='text-gray-800 text-hover-primary fs-2 fw-bolder me-1'>
+                      {data.firstName}
+                    </a>
+                    <a href='#'>
+                      <KTIcon iconName='verify' className='fs-1 text-primary' />
+                    </a>
+                    <a
+                      href='#'
+                      className='btn btn-sm btn-light-success fw-bolder ms-2 fs-8 py-1 px-3'
+                      data-bs-toggle='modal'
+                      data-bs-target='#kt_modal_upgrade_plan'
+                    >
+                      Upgrade to Pro
+                    </a>
+                  </div>
+
+                  <div className='d-flex flex-wrap fw-bold fs-6 mb-4 pe-2'>
+                    <a
+                      href='#'
+                      className='d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2'
+                    >
+                      <KTIcon iconName='profile-circle' className='fs-4 me-1' />
+                      Developer
+                    </a>
+                    <a
+                      href='#'
+                      className='d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2'
+                    >
+                      <KTIcon iconName='geolocation' className='fs-4 me-1' />
+                      SF, Bay Area
+                    </a>
+                    <a
+                      href='#'
+                      className='d-flex align-items-center text-gray-400 text-hover-primary mb-2'
+                    >
+                      <KTIcon iconName='sms' className='fs-4 me-1' />
+                      max@kt.com
+                    </a>
+                  </div>
+                </div>
+
+                <div className='d-flex my-4'>
+                  <a href='#' className='btn btn-sm btn-light me-2' id='kt_user_follow_button'>
+                    <KTIcon iconName='check' className='fs-3 d-none' />
+
+                    <span className='indicator-label'>Follow</span>
+                    <span className='indicator-progress'>
+                      Please wait...
+                      <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
+                    </span>
+                  </a>
+                  <a
+                    href='#'
+                    className='btn btn-sm btn-primary me-3'
+                    data-bs-toggle='modal'
+                    data-bs-target='#kt_modal_offer_a_deal'
+                  >
+                    Hire Me
+                  </a>
+                  <div className='me-0'>
+                    <button
+                      className='btn btn-sm btn-icon btn-bg-light btn-active-color-primary'
+                      data-kt-menu-trigger='click'
+                      data-kt-menu-placement='bottom-end'
+                      data-kt-menu-flip='top-end'
+                    >
+                      <i className='bi bi-three-dots fs-3'></i>
+                    </button>
+                    <Dropdown1 />
+                  </div>
+                </div>
+              </div>
+
+              <div className='d-flex flex-wrap flex-stack'>
+                <div className='d-flex flex-column flex-grow-1 pe-8'>
+                  <div className='d-flex flex-wrap'>
+                    <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                      <div className='d-flex align-items-center'>
+                        <KTIcon iconName='arrow-up' className='fs-3 text-success me-2' />
+                        <div className='fs-2 fw-bolder'>4500$</div>
+                      </div>
+
+                      <div className='fw-bold fs-6 text-gray-400'>Earnings</div>
+                    </div>
+
+                    <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                      <div className='d-flex align-items-center'>
+                        <KTIcon iconName='arrow-down' className='fs-3 text-danger me-2' />
+                        <div className='fs-2 fw-bolder'>75</div>
+                      </div>
+
+                      <div className='fw-bold fs-6 text-gray-400'>Projects</div>
+                    </div>
+
+                    <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3'>
+                      <div className='d-flex align-items-center'>
+                        <KTIcon iconName='arrow-up' className='fs-3 text-success me-2' />
+                        <div className='fs-2 fw-bolder'>60%</div>
+                      </div>
+
+                      <div className='fw-bold fs-6 text-gray-400'>Success Rate</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='d-flex align-items-center w-200px w-sm-300px flex-column mt-3'>
+                  <div className='d-flex justify-content-between w-100 mt-auto mb-2'>
+                    <span className='fw-bold fs-6 text-gray-400'>Profile Compleation</span>
+                    <span className='fw-bolder fs-6'>50%</span>
+                  </div>
+                  <div className='h-5px mx-3 w-100 bg-light mb-3'>
+                    <div
+                      className='bg-success rounded h-5px'
+                      role='progressbar'
+                      style={{width: '50%'}}
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='d-flex overflow-auto h-55px'>
+            <ul className='nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder flex-nowrap'>
+              <li className='nav-item'>
+                <Link
+                  className={
+                    `nav-link text-active-primary me-6 ` +
+                    (location.pathname === '/crafted/account/overview' && 'active')
+                  }
+                  to='/crafted/account/overview'
+                >
+                  Overview
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  className={
+                    `nav-link text-active-primary me-6 ` +
+                    (location.pathname === '/crafted/account/settings' && 'active')
+                  }
+                  to='/crafted/account/settings'
+                >
+                  Settings
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className='card mb-5 mb-xl-10' id='kt_profile_details_view'>
+        <div className='card-header cursor-pointer'>
+          <div className='card-title m-0'>
+            <h3 className='fw-bolder m-0'>Profile Details</h3>
+          </div>
+
+          <Link to='/crafted/account/settings' className='btn btn-primary align-self-center'>
+            Edit Profile
+          </Link>
+        </div>
+
+        <div className='card-body p-9'>
+          <div className='row mb-7'>
+            <label className='col-lg-4 fw-bold text-muted'>Ime i prezime</label>
+
+            <div className='col-lg-8'>
+              <span className='fw-bolder fs-6 text-dark'>{data.firstName} {data.lastName}</span>
+            </div>
+          </div>
+
+          <div className='row mb-7'>
+            <label className='col-lg-4 fw-bold text-muted'>Adresa</label>
+
+            <div className='col-lg-8 fv-row'>
+              <span className='fw-bold fs-6'>{data.address}</span>
+            </div>
+          </div>
+
+          <div className='row mb-7'>
+            <label className='col-lg-4 fw-bold text-muted'>
+              {data.mob}
+              <i
+                className='fas fa-exclamation-circle ms-1 fs-7'
+                data-bs-toggle='tooltip'
+                title='Phone number must be active'
+              ></i>
+            </label>
+
+            <div className='col-lg-8 d-flex align-items-center'>
+              <span className='fw-bolder fs-6 me-2'>044 3276 454 935</span>
+
+              <span className='badge badge-success'>Verified</span>
+            </div>
+          </div>
+
+          <div className='row mb-7'>
+            <label className='col-lg-4 fw-bold text-muted'>Company Site</label>
+
+            <div className='col-lg-8'>
+              <a href='#' className='fw-bold fs-6 text-dark text-hover-primary'>
+                keenthemes.com
+              </a>
+            </div>
+          </div>
+
+          <div className='row mb-7'>
+            <label className='col-lg-4 fw-bold text-muted'>
+              Country
+              <i
+                className='fas fa-exclamation-circle ms-1 fs-7'
+                data-bs-toggle='tooltip'
+                title='Country of origination'
+              ></i>
+            </label>
+
+            <div className='col-lg-8'>
+              <span className='fw-bolder fs-6 text-dark'>Germany</span>
+            </div>
+          </div>
+
+          <div className='row mb-7'>
+            <label className='col-lg-4 fw-bold text-muted'>Communication</label>
+
+            <div className='col-lg-8'>
+              <span className='fw-bolder fs-6 text-dark'>Email, Phone</span>
+            </div>
+          </div>
+
+          <div className='row mb-10'>
+            <label className='col-lg-4 fw-bold text-muted'>Allow Changes</label>
+
+            <div className='col-lg-8'>
+              <span className='fw-bold fs-6'>Yes</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='row gy-10 gx-xl-10'>
+        <div className='col-xl-6'>
+          <ChartsWidget1 className='card-xxl-stretch mb-5 mb-xl-10' />
+        </div>
+
+        <div className='col-xl-6'>
+          <TablesWidget1 className='card-xxl-stretch mb-5 mb-xl-10' />
+        </div>
+      </div>
+
+      <div className='row gy-10 gx-xl-10'>
+        <div className='col-xl-6'>
+          <ListsWidget5 className='card-xxl-stretch mb-5 mb-xl-10' />
+        </div>
+
+        <div className='col-xl-6'>
+          <TablesWidget5 className='card-xxl-stretch mb-5 mb-xl-10' />
+        </div>
+      </div>
   
 
 
